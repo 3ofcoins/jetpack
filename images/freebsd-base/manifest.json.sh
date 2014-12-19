6ad8e87a1e8debad01313670b0ac12843ca96aa2
Maciej Pasternacki <maciej@3ofcoins.net>
2014-12-19 14:47:23 +0100

    Drop now-unnecessary JSON fragments

--- images/freebsd-base/manifest.json.sh
+++ images/freebsd-base/manifest.json.sh
@@ -3,20 +3,14 @@ set -e
 
 version="$(freebsd-version -u | sed 's/-[A-Z][A-Z]*-p/./')"
 arch="$(uname -m)"
-timestamp="$(date -u '+%FT%TZ')"
 
 cat <<EOF
 {
-  "acKind": "ImageManifest",
-  "acVersion": "0.1.1",
   "name": "freebsd-base",
   "labels": [
     { "name": "version", "val": "${version}" },
     { "name": "os", "val": "freebsd" },
     { "name": "arch", "val": "${arch}" }
-  ],
-  "annotations": {
-    "created": "${timestamp}"
-  }
+  ]
 }
 EOF
