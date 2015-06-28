abfb3126150a25b8baa4582c7b0342c2fe23750f
Maciej Pasternacki <maciej@3ofcoins.net>
2015-06-28 17:04:47 +0200

    Unbreak `jetpack init` (fixes #10)

--- src/bin/jetpack/host.go
+++ src/bin/jetpack/host.go
@@ -7,7 +7,7 @@ import (
 )
 
 func init() {
-	AddCommand("init", "Initialize host", cmdWrapErr(Host.Initialize), nil)
+	AddCommand("init", "Initialize host", cmdWrapErr(cmdInit), nil)
 	AddCommand("config", "Show configuration", cmdWrap(cmdConfig), nil)
 }
 
@@ -15,4 +15,8 @@ func cmdConfig() {
 	lines := strings.Split(Host.Properties.String(), "\n")
 	sort.Strings(lines)
 	fmt.Println(strings.Join(lines[1:], "\n")) // first "line" is empty due to trailing newline
+}
+
+func cmdInit() error {
+	return Host.Initialize()
 }
