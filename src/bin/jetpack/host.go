5cc6ecff214ac6298d32587f4f5986f3fb3e9b5e
Maciej Pasternacki <maciej@3ofcoins.net>
2015-06-30 15:42:31 +0200

    `jetpack config` can now print single values

--- src/bin/jetpack/host.go
+++ src/bin/jetpack/host.go
@@ -4,17 +4,30 @@ import (
 	"fmt"
 	"sort"
 	"strings"
+
+	"github.com/juju/errors"
 )
 
 func init() {
 	AddCommand("init", "Initialize host", cmdWrapErr(cmdInit), nil)
-	AddCommand("config", "Show configuration", cmdWrap(cmdConfig), nil)
+	AddCommand("config [VAR...]", "Show configuration", cmdConfig, nil)
 }
 
-func cmdConfig() {
-	lines := strings.Split(Host.Properties.String(), "\n")
-	sort.Strings(lines)
-	fmt.Println(strings.Join(lines[1:], "\n")) // first "line" is empty due to trailing newline
+func cmdConfig(args []string) error {
+	if len(args) == 0 {
+		lines := strings.Split(Host.Properties.String(), "\n")
+		sort.Strings(lines)
+		fmt.Println(strings.Join(lines[1:], "\n")) // first "line" is empty due to trailing newline
+	} else {
+		for _, propName := range args {
+			if val, ok := Host.Properties.Get(propName); ok {
+				fmt.Println(val)
+			} else {
+				return errors.Errorf("No such property: %v", propName)
+			}
+		}
+	}
+	return nil
 }
 
 func cmdInit() error {
